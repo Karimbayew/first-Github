b2164b2bd3de2d00cc2a4c1d6d36d42ef6386852
shahzad <karimbayewshahzad@gmail.com>
2022-12-08 02:08:10 +0500

    uceba

--- script.js
+++ script.js
@@ -1,6 +1,69 @@
 "use strict";
 
-const isChecked = true,
-      isClose = false;
+// let num = 40;
+// (num == 50) ? console.log('ok') : console.log('error');
 
-console.log(isChecked || isClose);
+// let num = 49;
+
+// switch (num) {
+//     case 50:
+//         console.log('dogry');
+//         break;
+//     case 49: 
+//         console.log('yakyn');
+//         break;
+//     case 48: 
+//         console.log('yakynrak');
+//         break;
+//     default:
+//         console.log('nadoogry');
+//         break;
+// }
+
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries) {
+//     console.log('men dok');
+// } else if (hamburger || fries) {
+//     console.log('men norm');
+// } else {
+//     console.log('ac');
+// }
+
+// console.log( NaN || 2 || undefined ); // +
+// console.log( NaN && 2 && undefined ); 
+// console.log( 1 && 2 && 3 ); // +
+// console.log( !1 && 2 || !3 );
+// console.log( 25 || null && !3 );
+// console.log( NaN || null || !3 || undefined || 5); // +
+// console.log( NaN || null && !3 && undefined || 5); // +
+// console.log( 5 === 5 && 3 > 1 || 5); // +
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!');
+// }
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+} 
